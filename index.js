1f76de623e4d1f3111ac31d444503c1758cd7593
storngfornt <au0038173@gmail.com>
2024-04-27 23:40:06 +0000

    apply sorting function for sort data from db

--- index.js
+++ index.js
@@ -34,6 +34,11 @@ async function run() {
         const result = await cursor.toArray()
         res.send(result);
       });
+      app.get("/touristSpotSort",async(req,res)=>{
+        const cursor = spotCollection.find().sort({"cost":1})
+        const result = await cursor.toArray()
+        res.send(result)
+      })
 
      app.post("/touristSpot",async(req,res)=>{
         const spot = req.body;
