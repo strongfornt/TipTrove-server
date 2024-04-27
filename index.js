ecfbf2fff1d750cfc11a6460a9aa08857213dc08
storngfornt <au0038173@gmail.com>
2024-04-27 12:46:57 +0000

    get data from database

--- index.js
+++ index.js
@@ -29,8 +29,10 @@ async function run() {
     app.get("/", (req, res) => {
       res.send("server is running");
     });
-    app.get("/touristSpot", (req, res) => {
-        res.send("Tourist is running");
+    app.get("/touristSpot",async (req, res) => {
+        const cursor = spotCollection.find()
+        const result = await cursor.toArray()
+        res.send(result);
       });
 
      app.post("/touristSpot",async(req,res)=>{
@@ -44,7 +46,7 @@ async function run() {
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
+      "Pinged your deployment. You  connected to MongoDB!"
     );
   } finally {
     // Ensures that the client will close when you finish/error
