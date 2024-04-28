b0e3e23bf0b587cc0e061f725dd1267992cc46c4
storngfornt <au0038173@gmail.com>
2024-04-28 22:08:22 +0000

    added delete method for delet dataa from db

--- index.js
+++ index.js
@@ -86,7 +86,12 @@ async function run() {
         res.send(result)
      })
 
-
+     app.delete("/touristSpot/:id",async(req,res)=>{
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)}
+        const result = await spotCollection.deleteOne(query)
+        res.send(result)
+     })
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
     // console.log(
